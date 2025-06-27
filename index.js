5290bf56a62eb22523119640b4f3c51cf6c03b05
ogzacky75 <ogzacky75@gmail.com>
2025-06-27 16:49:12 +0300

    codegrade

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here!
+const deleteMain = document.querySelector('#main')
+
+deleteMain.remove()
+
+const newHeader = document.createElement('h1')
+
+document.body.append(newHeader)
+
+newHeader.id="victory"
+
+
+function headName(name){
+
+	let headName = document.getElementById('victory')
+	headName.textContent= `${name} is the champion` 
+
+}
+headName('brian')
